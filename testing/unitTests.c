76f9eaeda69a2bc9b5dae2d71be8775bae796d5a
JoeManto <joemanto123@gmail.com>
2019-11-07 17:54:54 -0500

    added arg logic for running different unit tests

--- testing/unitTests.c
+++ testing/unitTests.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc,char**argv){
-
+    int func = atoi(argv[0]);
+    if(!func){
+        if(strcmp(argv[0],"all")){
+            //run all the tests
+        }
+    }else{
+        switch (func)
+        {
+        case 0:
+            //run all unit test for a function
+            break;
+        default:
+            break;
+        }
+    }
 }
